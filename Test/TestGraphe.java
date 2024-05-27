289ef356c4ad0c5d4250a251ab22dbd212532d22
Alexandre014 <alexandre.cosson3@etu.univ-lorraine.fr>
2024-05-27 18:01:08 +0200

    creation classe Test

--- Test/TestGraphe.java
+++ Test/TestGraphe.java
@@ -3,6 +3,9 @@ import org.junit.jupiter.api.Test;
 public class TestGraphe {
     @Test
     public void test_GrapheListe_constructeur(){
+        GrapheListe grapheListe = new GrapheListe();
 
+        grapheListe.ajouterArc("A", "B", 3);
+        grapheListe.listeNoeuds();
     }
 }
