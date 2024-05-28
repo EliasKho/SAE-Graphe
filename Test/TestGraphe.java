65d526883d0ddc91ad83cd8b9d61584dfe168048
Alexandre014 <alexandre.cosson3@etu.univ-lorraine.fr>
2024-05-28 14:25:20 +0200

    maj de la structure des tests

--- Test/TestGraphe.java
+++ Test/TestGraphe.java
@@ -7,32 +7,32 @@ public class TestGraphe {
     public void test_GrapheListe_ajouterArc_OK(){
         GrapheListe grapheListe = new GrapheListe();
         grapheListe.ajouterArc("A", "B", 3);
-        assertEquals(grapheListe.listeNoeuds().size(), 1, "il doit y avoir 1 arc");
-        assertEquals(grapheListe.listeNoeuds().getFirst(), "A");
-        //bug
+        assertEquals(2, grapheListe.listeNoeuds().size(), "il doit y avoir 2 noeuds");
+        assertEquals("A", grapheListe.listeNoeuds().get(0));
+        assertEquals("B", grapheListe.listeNoeuds().get(1));
 
     }
     @Test
     public void test_GrapheListe_ajouterArc_arcsVides(){
         GrapheListe grapheListe = new GrapheListe();
-        assertEquals(grapheListe.listeNoeuds().size(), 0, "il doit y avoir 0 arc");
+        assertEquals(0, grapheListe.listeNoeuds().size(), "il doit y avoir 0 arc");
     }
     @Test
     public void test_GrapheListe_getIndice(){
         GrapheListe grapheListe = new GrapheListe();
         grapheListe.ajouterArc("A", "B", 3);
-        assertEquals(grapheListe.getIndice("A"), 0, "l'indice du noeud A doit être 0");
-        assertEquals(grapheListe.getIndice("B"), 1, "l'indice du noeud B doit être 1");
+        assertEquals(0, grapheListe.getIndice("A"), "l'indice du noeud A doit être 0");
+        assertEquals(1,grapheListe.getIndice("B") , "l'indice du noeud B doit être 1");
     }
     @Test
     public void test_GrapheListe_suivants(){
         GrapheListe grapheListe = new GrapheListe();
         grapheListe.ajouterArc("A", "B", 3);
 
-        assertEquals(grapheListe.getIndice("A"), 0, "l'indice du noeud A doit être 0");
-        assertEquals(grapheListe.getIndice("B"), 1, "l'indice du noeud B doit être 1");
+        assertEquals(0, grapheListe.getIndice("A"), "l'indice du noeud A doit être 0");
+        assertEquals(1, grapheListe.getIndice("B"), "l'indice du noeud B doit être 1");
 
-        assertEquals(grapheListe.suivants("A").getFirst().getDest(), "B", "le successeur de A doit être B");
+        assertEquals("B", grapheListe.suivants("A").get(0).getDest(), "le successeur de A doit être B");
     }
 
 
