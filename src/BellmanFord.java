57e397b14f545db69459a49901d428d01296fe7f
khodjaou2u <elias.khodjaoui@free.fr>
2024-05-29 09:53:29 +0200

    completion classe BellmanFord

--- src/BellmanFord.java
+++ src/BellmanFord.java
@@ -2,28 +2,27 @@ public class BellmanFord {
 
     public static Valeur resoudre(Graphe g, String depart) {
         Valeur valeur = new Valeur();
-        for (String noeud : g.listeNoeuds()) {
-            valeur.setValeur(noeud, Double.MAX_VALUE);
-            valeur.setParent(noeud, null);
+        for (int i=0;i<g.listeNoeuds().size();i++) {
+            valeur.setValeur(g.listeNoeuds().get(i), Double.MAX_VALUE);
+            valeur.setParent(g.listeNoeuds().get(i), null);
         }
         valeur.setValeur(depart, 0);
 
-        boolean changed;
-        do {
-            changed = false;
-            for (String u : g.listeNoeuds()) {
-                for (Arc arc : g.suivants(u)) {
-                    String v = arc.getDest();
-                    double cout = arc.getCout();
-                    if (valeur.getValeur(u) + cout < valeur.getValeur(v)) {
-                        valeur.setValeur(v, valeur.getValeur(u) + cout);
-                        valeur.setParent(v, u);
-                        changed = true;
+        boolean changer=true;
+        while(changer){
+            changer = false;
+            for (int i=0;i<g.listeNoeuds().size();i++) {
+                for (int j=0;j<g.suivants(g.listeNoeuds().get(i)).size();j++) {
+                    String v = g.suivants(g.listeNoeuds().get(i)).get(j).getDest();
+                    double cout = g.suivants(g.listeNoeuds().get(i)).get(j).getCout();
+                    if (valeur.getValeur(g.listeNoeuds().get(i)) + cout < valeur.getValeur(v)) {
+                        valeur.setValeur(v, valeur.getValeur(g.listeNoeuds().get(i)) + cout);
+                        valeur.setParent(v, g.listeNoeuds().get(i));
+                        changer = true;
                     }
                 }
             }
-        } while (changed);
-
+        }
         return valeur;
     }
 }
