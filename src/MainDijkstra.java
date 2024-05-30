a6e40003afc865b43596983e658e0b03371199be
Alexandre014 <alexandre.cosson3@etu.univ-lorraine.fr>
2024-05-30 09:17:34 +0200

    main

--- src/MainDijkstra.java
+++ src/MainDijkstra.java
@@ -8,9 +8,20 @@ public class MainDijkstra {
         g.ajouterArc("E","D",5);
         g.ajouterArc("C","E",3);
 
+        long dureeTotale = 0;
+        long date_debut;
+        long date_fin;
+
+        date_debut = System.nanoTime();
         Valeur vObtenue = Dijkstra.resoudre(g, "A");
 
         System.out.println(vObtenue);
+        date_fin = System.nanoTime();
+
+        dureeTotale += date_fin - date_debut;//duree en nanosecondes
+
+        System.out.println(dureeTotale*10E-9);//duree en secondes
+
     }
 
 }
